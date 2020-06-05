75d92630311fb8c3100ae982516003a83854af7b
Paulo Ariel Pareja <paulo.pareja@hotmail.es>
2020-06-04 23:18:21 -0300

    initial config rabbitmq

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Paulo-Ariel-Pareja/go-and-rabbitmq-simple-example
+
+go 1.14
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stradway/amqp"
+)
 
 func main() {
-	fmt.Println("Tutorial Rabbitmq")
+	fmt.Println("Ejemplo con Go y Rabbitmq")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nul {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
 }
